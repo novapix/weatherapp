b77a969bd954cdd68529df789760618693d65d98
Parakram <89983841+Parakramk@users.noreply.github.com>
2024-01-10 19:18:32 +0545

    handle 404 error separately and convert response string to QJson

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -24,6 +24,11 @@ void MainWindow::fetchWeather() {
   const std::string baseUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
   std::string apiURL = std::format("{}{}&appid={}", baseUrl, city, apiKey);
   cpr::Response res = cpr::Get(cpr::Url{apiURL});
-  if (res.error || res.status_code == 404) {
+  if (res.error) {
+    if (res.status_code == 404) {
+      QMessageBox::warning(this, "Error", "City not Found");
+    }
+    QMessageBox::warning(this, "Error", "failed to fetch weather data");
   }
+  QJsonDocument jsonRes = QJsonDocument::fromJson(res.text.c_str());
 }
