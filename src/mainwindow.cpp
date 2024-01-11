2187b0c43c5fc9c3bb73bf5445d97c95feb6fa39
Parakram <89983841+Parakramk@users.noreply.github.com>
2024-01-11 20:36:12 +0545

    add Max/Min Temp and Source code Organization

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,16 +1,19 @@
-#include "include/mainwindow.h"
+#include "../include/mainwindow.h"
 #include <QJsonDocument>
 #include <QJsonObject>
 #include <QJsonValue>
 #include <QMessageBox>
-#include "./include/ui_mainwindow.h"
+#include "../include/ui_mainwindow.h"
 #include <format>
+
+using std::string;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow) {
   ui->setupUi(this);
   connect(ui->btnFetch, &QPushButton::clicked, this, &MainWindow::fetchWeather);
+  // connect(ui->btnFetch, SIGNAL(returnPressed()), this, &MainWindow::fetchWeather);
 }
 
 MainWindow::~MainWindow() {
@@ -19,18 +22,18 @@ MainWindow::~MainWindow() {
 
 void MainWindow::fetchWeather() {
   QString cityInput = ui->cityInput->text();
-  std::string city = cityInput.toStdString();
-  const std::string apiKey = "";
+  string city = cityInput.toStdString();
+  const string apiKey = "x";
   if (apiKey.empty()) {
     QMessageBox::warning(this, "API ERROR", "Please Add API KEY");
   } else {
-    const std::string baseUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
-    std::string reqURL = std::format("{}{}&appid={}", baseUrl, city, apiKey);
+    const string baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+    string reqURL = std::format("{}{}&appid={}", baseUrl, city, apiKey);
     cpr::Response res = cpr::Get(cpr::Url{reqURL});
     if (res.status_code == 404) {
       QMessageBox::warning(this, "Error", "City not Found");
     }
-    if (res.error) {
+    if (res.status_code != 200) {
       QMessageBox::warning(this, "Error", "failed to fetch weather data");
     }
     QJsonDocument jsonResponse = QJsonDocument::fromJson(res.text.c_str());
@@ -38,9 +41,16 @@ void MainWindow::fetchWeather() {
       // Get the "main" object and extract the temperature, using 0.0 as a default
       // if "temp" is not present
       double temperature = jsonResponse.object()["main"].toObject()["temp"].toDouble() - 273.15;
+      double maxTemp = jsonResponse.object()["main"].toObject()["temp_max"].toDouble() - 273.15;
+      double minTemp = jsonResponse.object()["main"].toObject()["temp_min"].toDouble() - 273.15;
 
-      // Set the temperature in the read-only QLineEdit
-      ui->tempOutput->setText(QString("Temperature: %1 °C").arg(temperature));
+
+      ui->tempDetailsText->setText(QString("Temperature: %1 °C\n"
+			    "Max Temp: %2 °C\n"
+			    "Min Temp: %3 °C")
+			.arg(temperature)
+			.arg(maxTemp)
+			.arg(minTemp));
     } else {
       QMessageBox::warning(this, "Error", "Invalid JSON document.");
     }
