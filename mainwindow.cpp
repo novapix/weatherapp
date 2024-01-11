c323dc6e2bbdae8502bffb9dbcd553a1a8e5f4f2
Parakram <89983841+Parakramk@users.noreply.github.com>
2024-01-11 19:02:07 +0545

    organize code structure

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,9 @@
-#include "mainwindow.h"
+#include "include/mainwindow.h"
 #include <QJsonDocument>
 #include <QJsonObject>
 #include <QJsonValue>
 #include <QMessageBox>
-#include "./ui_mainwindow.h"
+#include "./include/ui_mainwindow.h"
 #include <format>
 
 MainWindow::MainWindow(QWidget *parent)
@@ -23,25 +23,26 @@ void MainWindow::fetchWeather() {
   const std::string apiKey = "";
   if (apiKey.empty()) {
     QMessageBox::warning(this, "API ERROR", "Please Add API KEY");
-  }
-  const std::string baseUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
-  std::string reqURL = std::format("{}{}&appid={}", baseUrl, city, apiKey);
-  cpr::Response res = cpr::Get(cpr::Url{reqURL});
-  if (res.status_code == 404) {
-    QMessageBox::warning(this, "Error", "City not Found");
-  }
-  if (res.error) {
-    QMessageBox::warning(this, "Error", "failed to fetch weather data");
-  }
-  QJsonDocument jsonResponse = QJsonDocument::fromJson(res.text.c_str());
-  if (jsonResponse.isObject()) {
-    // Get the "main" object and extract the temperature, using 0.0 as a default
-    // if "temp" is not present
-    double temperature = jsonResponse.object()["main"].toObject()["temp"].toDouble() - 273.15;
-
-    // Set the temperature in the read-only QLineEdit
-    ui->tempOutput->setText(QString("Temperature: %1 °C").arg(temperature));
   } else {
-    QMessageBox::warning(this, "Error", "Invalid JSON document.");
+    const std::string baseUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+    std::string reqURL = std::format("{}{}&appid={}", baseUrl, city, apiKey);
+    cpr::Response res = cpr::Get(cpr::Url{reqURL});
+    if (res.status_code == 404) {
+      QMessageBox::warning(this, "Error", "City not Found");
+    }
+    if (res.error) {
+      QMessageBox::warning(this, "Error", "failed to fetch weather data");
+    }
+    QJsonDocument jsonResponse = QJsonDocument::fromJson(res.text.c_str());
+    if (jsonResponse.isObject()) {
+      // Get the "main" object and extract the temperature, using 0.0 as a default
+      // if "temp" is not present
+      double temperature = jsonResponse.object()["main"].toObject()["temp"].toDouble() - 273.15;
+
+      // Set the temperature in the read-only QLineEdit
+      ui->tempOutput->setText(QString("Temperature: %1 °C").arg(temperature));
+    } else {
+      QMessageBox::warning(this, "Error", "Invalid JSON document.");
+    }
   }
 }
