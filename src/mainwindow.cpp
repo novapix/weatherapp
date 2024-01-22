aac80659c8102b07d14f296bf066e377e7f9f6ea
Parakram <89983841+Parakramk@users.noreply.github.com>
2024-01-22 17:19:43 +0545

    Use QLabel instead of QTextEdit

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -78,17 +78,17 @@ void MainWindow::updateWeather(const QJsonDocument &jsonRes) {
   double minTemp = jsonRes.object()["main"].toObject()["temp_min"].toDouble() - 273.15;
   ui->presentLabel->setText(QString("Current Weather"));
 //  ui->tempDetailsText->setAlignment(Qt::AlignCenter);
-  ui->tempDetailsText->setText(QString("Temperature: %1 °C\n"
-									   "Max Temp: %2 °C\n"
-									   "Min Temp: %3 °C")
-								   .arg(temperature)
-								   .arg(maxTemp)
-								   .arg(minTemp));
+  ui->tempDetails->setText(QString("Temperature: %1 °C\n"
+								   "Max Temp: %2 °C\n"
+								   "Min Temp: %3 °C")
+							   .arg(temperature)
+							   .arg(maxTemp)
+							   .arg(minTemp));
 
 }
 void MainWindow::clearResults() {
   ui->backgroundLabel->clear();
-  ui->tempDetailsText->clear();
+  ui->tempDetails->clear();
   ui->presentLabel->clear();
   ui->cityInput->clear();
 }
